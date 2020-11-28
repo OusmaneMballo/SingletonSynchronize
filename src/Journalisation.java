359f946e843fe3f8024fefb80a4f6fcb7896257c
OusmaneMballo <ousmanemballo576@gmail.com>
2020-11-28 16:57:41 +0000

    Creation de classe Journalisation qui est la classe singleton

---
+++ src/Journalisation.java
@@ -0,0 +1,33 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Journalisation {
+    private static Journalisation _instance;
+    private String journal; //Chaine de caractères représentant les messages de log.
+
+    private Journalisation(){
+        this.journal= "";
+    }
+
+    public static synchronized Journalisation getInstance(){
+        if (_instance==null){
+            _instance=new Journalisation();
+        }
+        return _instance;
+    }
+
+    // Méthode qui permet d'ajouter un message de log.
+    public void ajouterLog(String journal)
+    {
+        // On ajoute également la date du message.
+        Date d = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yy HH'h'mm");
+        this.journal+="["+dateFormat.format(d)+"] "+journal+"\n";
+    }
+    // Méthode qui retourne tous les messages de log.
+    public String afficherLog()
+    {
+        return journal;
+    }
+}
